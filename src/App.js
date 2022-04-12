388d670bae0d10023ffbeafca030c2b5f8edf8ff
Stephen Robbins <swrobbin@gmail.com>
2022-04-12 13:41:10 -0400

    basic header CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello world!</p>
+    <div className="container">
+      <h1 className='title'>React Movie Search</h1>
     </div>
   );
 }
