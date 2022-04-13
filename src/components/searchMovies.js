f6b73fbd0f2e7bbd104e40e4c955126782f62a2e
Stephen Robbins <swrobbin@gmail.com>
2022-04-13 12:58:49 -0400

    added fetch call to API + state to store search result

--- src/components/searchMovies.js
+++ src/components/searchMovies.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const SearchMovies = () => {
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([])
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    console.log("submitting");
+
+    // const query = "Jurassic Park";
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=d37e61442ae63b6487976f502df477b1&language=en-US&query=${query}&page=1&include_adult=false`;
+    
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results)
+      console.log(data.results);
+    }catch(err){
+      console.error(err)
+    }
+  } 
+
   return (
     <div>
-        <form className='form'>
+        <form className='form' onSubmit={searchMovies} >
             <label className='label' htmlFor="query">Movie Name</label>
-            <input className="input" type='text' name="query" placeholder="i.e. Jurassic Park"/>
+            <input value={query} onChange={(e) =>  setQuery(e.target.value)}className="input" type='text' name="query" placeholder="i.e. Jurassic Park"/>
             <button className="button" type='submit'>Search</button>
         </form>
     </div>
