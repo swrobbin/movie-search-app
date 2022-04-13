f8eb8f7897f7a11debc693bc6572547b59ebc0dc
Stephen Robbins <swrobbin@gmail.com>
2022-04-13 13:06:10 -0400

    card display

--- src/components/searchMovies.js
+++ src/components/searchMovies.js
@@ -24,13 +24,26 @@ const SearchMovies = () => {
   } 
 
   return (
-    <div>
+    <>
+
+     <div>
         <form className='form' onSubmit={searchMovies} >
             <label className='label' htmlFor="query">Movie Name</label>
             <input value={query} onChange={(e) =>  setQuery(e.target.value)}className="input" type='text' name="query" placeholder="i.e. Jurassic Park"/>
             <button className="button" type='submit'>Search</button>
         </form>
     </div>
+    <div className="card-list">
+      {movies.map(movie => (
+        <div className="card">
+          <img className="card--image" 
+          src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} 
+          alt={movie.title + " poster"} />
+        </div>
+      ))}
+    </div>
+    </>
+   
   )
 }
 
